917c51d08ce18d8e026ef6acc22f99b2d46f3a26
Anderson S. Vieira <svieira.anderson@gmail.com>
2021-10-04 15:37:26 -0300

    Exercícios do cap_02, antes de começar strings

---
+++ cap_02/simple_message.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+mensagem = "Este é um simples texto de exemplo."
+print(mensagem)
---
+++ cap_02/simple_messages.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+mensagem = "Este é um simples texto de exemplo."
+print(mensagem)
+
+mensagem = "Agora atualizei a variável com outro texto."
+print(mensagem)
